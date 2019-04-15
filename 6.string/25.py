2c3c4977bb86e8b18ba54d7c16465385bb237325
monkeybuzinis <46678413+monkeybuzinis@users.noreply.github.com>
2019-04-15 11:48:20 +0700

    Update 25.py

--- 6.string/25.py
+++ 6.string/25.py
@@ -26,21 +26,6 @@ for j in range (len(v)*2):
             s=s[:i]+' '+s[i:]
 if v[0]=="*":
     v=v[1:]
-for i in range (len(v)+1):
-    if s[i]==")" and s[i+1]!=" ":
-        v=v[:i+1]+'*'+v[i+1:]
-        s=s[:i+1]+' '+s[i+1:]
-    
-    elif s[i]!=" " and s[i+1]=='(':
-        v=v[:i+1]+'*'+v[i+1:]
-        s=s[:i+1]+' '+s[i+1:]
-        
-    elif (s[i].isalpha() and (s[i+1] not in " *)")):
-        v=v[:i+1]+'*'+v[i+1:]
-        s=s[:i+1]+' '+s[i+1:]
-        
-    elif ((s[i] not in " *(") and s[i+1].isalpha()):
-        v=v[:i+1]+'*'+v[i+1:]
-        s=s[:i+1]+' '+s[i+1:]
+
 
 print(v)
