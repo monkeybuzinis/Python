d8c6be2e88d1cd1ac5a95cb7dfcb09fb7725dff7
Khanh Kim Le <46678413+monkeybuzinis@users.noreply.github.com>
2022-07-03 11:04:36 +0700

    Update 12.py

--- 9.while loop/12.py
+++ 9.while loop/12.py
@@ -3,6 +3,7 @@ Rock-Paper-Scissors against the computer. In that program there were exactly fiv
 Rewrite the program so that it is a best 3 out of 5. That is, the first player to win three times is
 the winner.
 """
+
 list=['scissors','rock','paper']
 import random
 
@@ -10,21 +11,44 @@ you_win=0
 computer_win=0
 i=0
 
-while i<3:
+while i<5:
+    if you_win==3 or computer_win==3:
+        
+        if you_win==3:
+            print("you win :)")
+        else:
+            print("you lose :(")
+        break
+
     computer=random.choice(list)
-    print(i+1,":")
+    print("round ",i+1,":")
     yourturn=input("let's play: ")
-    while(yourturn==computer or yourturn not in list):
+            
+    while(yourturn not in list):
         yourturn=input("oops! play again: ")
-    if((yourturn==list[0] and computer==list[2]) or (yourturn==list[1] and computer==list[0])or(yourturn==list[2] and computer==list[1])): 
-        you_win+=1
-        i+=1
+        
     else:
-        computer_win+=1
-        i+=1
+        if yourturn==computer:
+            print("computer choice:",computer)
+            print("noone win this round")
+
+        elif((yourturn==list[0] and computer==list[2]) or (yourturn==list[1] and computer==list[0])or(yourturn==list[2] and computer==list[1])): 
+            print("computer choice:",computer)
+            you_win+=1
+            print("you win this round")
+            
+        else:
+            print("computer choice:",computer)
+            computer_win+=1
+            print("you lose this round")
+
+        i+=1          
+            
 else:
-    print()
+    print("Final:")
     if you_win>computer_win:
         print("you WIN :)")
-    else:
+    elif computer_win>you_win:
         print("you LOSE :(")
+    else:
+        print("Noone win")
